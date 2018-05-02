718887cfe4563c77d537190cf953bced9c8f3c6d
Irwan Syahrir <irwan.syahrir@sparebank1.no>
2018-05-02 15:09:45 +0200

    Cleanup KtXMLSeriesTest

--- src/test/kotlin/hudson/plugins/plot/KtXMLSeriesTest.kt
+++ src/test/kotlin/hudson/plugins/plot/KtXMLSeriesTest.kt
@@ -1,6 +1,7 @@
 package hudson.plugins.plot
 
 import hudson.FilePath
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Ignore
@@ -35,177 +36,163 @@ class KtXMLSeriesTest : KtSeriesTestCase() {
     }
 
     fun testXMLSeries_WhenNodesSharingAParentHaveOneStringAndOneNumericContent_ThenCoalesceNodesToPointLabelledWithStringContent() {
-        // Create a new XML series.
+
         val xpath = "//UIAction/name|//UIAction/numCalls"
         val series = XMLSeries(TEST2_XML_FILE, xpath, "NODESET", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST2_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
-        assertEquals(4, points!!.size)
-        val map = HashMap<String, Double>()
+
+        assertThat(points).isNotNull
+        assertThat(points!!.size).isEqualTo(4)
+
+        val labelValueMap = HashMap<String, Double>()
         for (point in points) {
-            map[point.label] = java.lang.Double.parseDouble(point.yvalue)
+            labelValueMap[point.label] = java.lang.Double.parseDouble(point.yvalue)
         }
 
-        assertEquals(7, map["AxTermDataService.updateItem"]!!.toInt())
-        assertEquals(2, map["AxTermDataService.createEntity"]!!.toInt())
+        assertThat(labelValueMap["AxTermDataService.updateItem"]).isEqualTo(7.0)
+        assertThat(labelValueMap["AxTermDataService.createEntity"]).isEqualTo(2.0)
+
         testPlotPoints(points, 4)
     }
 
     fun testXMLSeries_NODE_TO_STRING_WhenNodesHaveNoContent_ThenCoalesceForAttributes() {
-        // Create a new XML series.
         val xpath = "//testcase[@name='testOne'] | //testcase[@name='testTwo'] | //testcase[@name='testThree']"
-
         val series = XMLSeries(TEST_XML_FILE, xpath, "NODESET", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
-        assertEquals(points!!.size, 3)
-        assertEquals(points[0].label, "testOne")
-        assertEquals(points[1].label, "testTwo")
-        assertEquals(points[2].label, "testThree")
+
+        assertThat(points).isNotNull
+        assertThat(points!!.size).isEqualTo(3)
+        assertThat(points[0].label).isEqualTo("testOne")
+        assertThat(points[1].label).isEqualTo("testTwo")
+        assertThat(points[2].label).isEqualTo("testThree")
+
         testPlotPoints(points, 3)
     }
 
     fun testXMLSeriesNodeset() {
-        // Create a new XML series.
         val xpath = "//testcase"
-
         val series = XMLSeries(TEST_XML_FILE, xpath, "NODESET", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
-        assertEquals(4, points!!.size)
-        assertEquals(points[0].label, "testOne")
-        assertEquals(points[1].label, "testTwo")
-        assertEquals(points[2].label, "testThree")
-        assertEquals(points[3].label, "testFour")
-        assertEquals(points[3].yvalue, "1234.56")
+
+        assertThat(points).isNotNull
+        assertThat(points!!.size).isEqualTo(4)
+        assertThat(points[0].label).isEqualTo("testOne")
+        assertThat(points[1].label).isEqualTo("testTwo")
+        assertThat(points[2].label).isEqualTo("testThree")
+        assertThat(points[3].label).isEqualTo("testFour")
+        assertThat(points[3].yvalue).isEqualTo("1234.56")
+
         testPlotPoints(points, 4)
     }
 
     fun testXMLSeries_WhenAllNodesAreNumeric_ThenPointsAreLabelledWithNodeName() {
-        // Create a new XML series.
         val xpath = "/results/testcase/*"
-
         val series = XMLSeries(TEST3_XML_FILE, xpath, "NODESET", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST3_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
-        assertEquals(2, points!!.size)
-        assertEquals(points[0].label, "one")
-        assertEquals(points[0].yvalue, "0.521")
+
+        assertThat(points).isNotNull
+        assertThat(points!!.size).isEqualTo(2)
+        assertThat(points[0].label).isEqualTo("one")
+        assertThat(points[0].yvalue).isEqualTo("0.521")
+
         testPlotPoints(points, 2)
     }
 
     fun testXMLSeriesEmptyNodeset() {
-        // Create a new XML series.
         val xpath = "/there/is/no/such/element"
-
         val series = XMLSeries(TEST3_XML_FILE, xpath, "NODESET", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST3_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
-        assertEquals(0, points!!.size)
+
+        assertThat(points).isNotNull
+        assertThat(points!!.size).isEqualTo(0)
+
         testPlotPoints(points, 0)
     }
 
     fun testXMLSeriesNode_ADD_NODE_TO_LIST() { //USE THIS
-        // Create a new XML series.
         val xpath = "//testcase[@name='testThree']"
         val series = XMLSeries(TEST_XML_FILE, xpath, "NODE", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
-        assertEquals(points!!.size, 1)
-        assertEquals(java.lang.Double.parseDouble(points[0].yvalue), 27.0)
+
+        assertThat(points).isNotNull
+        assertThat(points!!.size).isEqualTo(1)
+        assertThat(java.lang.Double.parseDouble(points[0].yvalue)).isEqualTo(27.0)
+
         testPlotPoints(points, 1)
     }
 
     fun testXMLSeriesString() {
-        // Create a new XML series.
         val xpath = "//testcase[@name='testOne']/@time"
         val series = XMLSeries(TEST_XML_FILE, xpath, "STRING", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
+
+        assertThat(points).isNotNull
+
         testPlotPoints(points, 1)
     }
 
     fun testXMLSeriesNumber() {
-        // Create a new XML series.
-        var xpath = "concat(//testcase[@name='testOne']/@name, '=', //testcase[@name='testOne']/@time)"
-        xpath = "//testcase[@name='testOne']/@time"
-        val series = XMLSeries(TEST_XML_FILE, xpath, "NUMBER",
-                "splunge")
+        val xpath = "//testcase[@name='testOne']/@time"
+        val series = XMLSeries(TEST_XML_FILE, xpath, "NUMBER","splunge")
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
+
+        assertThat(points).isNotNull
+
         testPlotPoints(points, 1)
     }
 
     fun testXMLSeriesUrl() {
-        // Create a new XML series.
         val xpath = "/results/testcase/*"
+        val url = "http://localhost/%build%/%name%/%index%"
+        val series = XMLSeries(TEST3_XML_FILE, xpath, "NODESET", url)
 
-        val series = XMLSeries(TEST3_XML_FILE, xpath, "NODESET",
-                "http://localhost/%build%/%name%/%index%")
-
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST3_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 42, System.out)
-        assertNotNull(points)
+
+        assertThat(points).isNotNull
+
         testPlotPoints(points, 2)
-        assertEquals("http://localhost/42/one/0", points!![0].url)
-        assertEquals("http://localhost/42/two/0", points[1].url)
+
+        assertThat(points!![0].url).isEqualTo("http://localhost/42/one/0")
+        assertThat(points[1].url).isEqualTo("http://localhost/42/two/0")
     }
 
     @Ignore
     fun testXMLSeriesBoolean() {
-        // Create a new XML series.
         val xpath = "//testcase[@name='testOne']/@time"
         val series = XMLSeries(TEST_XML_FILE, xpath, "BOOLEAN", null)
 
-        // test the basic subclass properties.
         testSeriesProperties(series, TEST_XML_FILE, "", "xml")
 
-        // load the series.
         val points = series.loadSeries(workspaceRootDir!!, 0, System.out)
-        assertNotNull(points)
+
+        assertThat(points).isNotNull
+
         testPlotPoints(points, 1)
     }
 }
